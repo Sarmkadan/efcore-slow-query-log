8c2303e4463c3ea348737c4ee687be38cc8888a0
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    claenup in EndToEndInterceptionTestsJsonExtensions

--- tests/EfCore.SlowQueryLog.Tests/EndToEndInterceptionTestsJsonExtensions.cs
+++ tests/EfCore.SlowQueryLog.Tests/EndToEndInterceptionTestsJsonExtensions.cs
@@ -17,35 +17,23 @@ public static class EndToEndInterceptionTestsJsonExtensions
     /// <param name="indented">Whether to indent the JSON for readability.</param>
     /// <returns>The JSON representation of the instance.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this EndToEndInterceptionTests value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-        var options = indented
-            ? new JsonSerializerOptions(_jsonSerializerOptions)
-            {
-                WriteIndented = true,
-            }
-            : _jsonSerializerOptions;
-        return JsonSerializer.Serialize(value, options);
-    }
+    public static string ToJson(this EndToEndInterceptionTests value, bool indented = false) =>
+        JsonSerializer.Serialize(value, indented ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true } : _jsonSerializerOptions);
 
     /// <summary>
     /// Deserializes a JSON string to a <see cref="EndToEndInterceptionTests"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized instance, or null if deserialization fails.</returns>
+    /// <returns>The deserialized instance.</returns>
     /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    public static EndToEndInterceptionTests? FromJson(string json)
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or whitespace.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is invalid or cannot be deserialized.</exception>
+    public static EndToEndInterceptionTests FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
-        try
-        {
-            return JsonSerializer.Deserialize<EndToEndInterceptionTests>(json, _jsonSerializerOptions);
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+        return JsonSerializer.Deserialize<EndToEndInterceptionTests>(json, _jsonSerializerOptions)
+            ?? throw new JsonException("Deserialized value is null");
     }
 
     /// <summary>
