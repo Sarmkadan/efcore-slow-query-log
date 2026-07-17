0491791e45a72b5e024ccda25295c5e19b9ffe51
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    refactor: small refactor - SlowQueryInterceptorJsonExtensions

--- src/EfCore.SlowQueryLog/Interception/SlowQueryInterceptorJsonExtensions.cs
+++ src/EfCore.SlowQueryLog/Interception/SlowQueryInterceptorJsonExtensions.cs
@@ -19,20 +19,22 @@ public static class SlowQueryInterceptorJsonExtensions
     /// <param name="value">The <see cref="SlowQueryInterceptor"/> instance to serialize.</param>
     /// <param name="indented">A value indicating whether the JSON string should be formatted with indentation.</param>
     /// <returns>A JSON string representing the <see cref="SlowQueryInterceptor"/> instance.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is <c>null</c>.</exception>
     public static string ToJson(this SlowQueryInterceptor value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-        return JsonSerializer.Serialize(value, _jsonSerializerOptions);
-    }
+        => JsonSerializer.Serialize(value, _jsonSerializerOptions);
 
     /// <summary>
     /// Deserializes a JSON string to a <see cref="SlowQueryInterceptor"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>A <see cref="SlowQueryInterceptor"/> instance representing the deserialized JSON string, or <c>null</c> if deserialization fails.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
     public static SlowQueryInterceptor? FromJson(string json)
     {
         ArgumentNullException.ThrowIfNull(json);
+        ArgumentException.ThrowIfNullOrEmpty(json, nameof(json));
+
         try
         {
             return JsonSerializer.Deserialize<SlowQueryInterceptor>(json, _jsonSerializerOptions);
@@ -49,9 +51,13 @@ public static class SlowQueryInterceptorJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">The deserialized <see cref="SlowQueryInterceptor"/> instance, or <c>null</c> if deserialization fails.</param>
     /// <returns>A value indicating whether the deserialization was successful.</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="json"/> is <c>null</c>.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is empty or whitespace.</exception>
     public static bool TryFromJson(string json, out SlowQueryInterceptor? value)
     {
         ArgumentNullException.ThrowIfNull(json);
+        ArgumentException.ThrowIfNullOrEmpty(json, nameof(json));
+
         try
         {
             value = JsonSerializer.Deserialize<SlowQueryInterceptor>(json, _jsonSerializerOptions);
