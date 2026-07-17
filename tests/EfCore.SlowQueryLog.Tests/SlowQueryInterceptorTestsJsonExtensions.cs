3f6b74f1bc1379f5cb1480633daf9a2373ebf58f
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Tidy up SlowQueryInterceptorTestsJsonExtensions

--- tests/EfCore.SlowQueryLog.Tests/SlowQueryInterceptorTestsJsonExtensions.cs
+++ tests/EfCore.SlowQueryLog.Tests/SlowQueryInterceptorTestsJsonExtensions.cs
@@ -1,72 +1,70 @@
 using System.Text.Json;
-using System.Text.Json.Serialization;
 
 namespace EfCore.SlowQueryLog.Tests;
 
+/// <summary>
+/// Provides JSON serialization and deserialization extensions for <see cref="SlowQueryInterceptorTests"/> test class instances.
+/// </summary>
 public static class SlowQueryInterceptorTestsJsonExtensions
 {
-    private static readonly JsonSerializerOptions _jsonSerializerOptions = new()
-    {
-        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-    };
+ private static readonly JsonSerializerOptions _jsonSerializerOptions = new(JsonSerializerDefaults.Web)
+ {
+ PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+ WriteIndented = false,
+ };
 
-    /// <summary>
-    /// Serializes a <see cref="SlowQueryInterceptorTests"/> instance to a JSON string.
-    /// </summary>
-    /// <param name="value">The instance to serialize.</param>
-    /// <param name="indented">Whether to indent the JSON for readability.</param>
-    /// <returns>The JSON representation of the instance.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this SlowQueryInterceptorTests value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-        var options = indented
-            ? new JsonSerializerOptions(_jsonSerializerOptions)
-            {
-                WriteIndented = true,
-            }
-            : _jsonSerializerOptions;
-        return JsonSerializer.Serialize(value, options);
-    }
+ /// <summary>
+ /// Serializes a <see cref="SlowQueryInterceptorTests"/> instance to a JSON string.
+ /// </summary>
+ /// <param name="value">The instance to serialize.</param>
+ /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+ /// <returns>A JSON string representation of the test class.</returns>
+ /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is <see langword="null"/>.</exception>
+ public static string ToJson(this SlowQueryInterceptorTests value, bool indented = false)
+ {
+ ArgumentNullException.ThrowIfNull(value);
 
-    /// <summary>
-    /// Deserializes a JSON string to a <see cref="SlowQueryInterceptorTests"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <returns>The deserialized instance, or null if deserialization fails.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    public static SlowQueryInterceptorTests? FromJson(string json)
-    {
-        ArgumentNullException.ThrowIfNull(json);
-        try
-        {
-            return JsonSerializer.Deserialize<SlowQueryInterceptorTests>(json, _jsonSerializerOptions);
-        }
-        catch (JsonException)
-        {
-            return null;
-        }
-    }
+ var options = indented
+ ? new JsonSerializerOptions(_jsonSerializerOptions) { WriteIndented = true }
+ : _jsonSerializerOptions;
 
-    /// <summary>
-    /// Attempts to deserialize a JSON string to a <see cref="SlowQueryInterceptorTests"/> instance.
-    /// </summary>
-    /// <param name="json">The JSON string to deserialize.</param>
-    /// <param name="value">The deserialized instance, or null if deserialization fails.</param>
-    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
-    public static bool TryFromJson(string json, out SlowQueryInterceptorTests? value)
-    {
-        ArgumentNullException.ThrowIfNull(json);
-        try
-        {
-            value = JsonSerializer.Deserialize<SlowQueryInterceptorTests>(json, _jsonSerializerOptions);
-            return true;
-        }
-        catch (JsonException)
-        {
-            value = null;
-            return false;
-        }
-    }
+ return JsonSerializer.Serialize(value, options);
+ }
+
+ /// <summary>
+ /// Deserializes a JSON string to a <see cref="SlowQueryInterceptorTests"/> instance.
+ /// </summary>
+ /// <param name="json">The JSON string to deserialize.</param>
+ /// <returns>The deserialized test class instance.</returns>
+ /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is <see langword="null"/>.</exception>
+ /// <exception cref="JsonException">Thrown when <paramref name="json"/> is invalid or cannot be deserialized.</exception>
+ public static SlowQueryInterceptorTests? FromJson(string json)
+ {
+ ArgumentNullException.ThrowIfNull(json);
+
+ return JsonSerializer.Deserialize<SlowQueryInterceptorTests>(json, _jsonSerializerOptions);
+ }
+
+ /// <summary>
+ /// Attempts to deserialize a JSON string to a <see cref="SlowQueryInterceptorTests"/> instance.
+ /// </summary>
+ /// <param name="json">The JSON string to deserialize.</param>
+ /// <param name="value">Receives the deserialized test class instance if successful.</param>
+ /// <returns><see langword="true"/> if deserialization succeeds; otherwise, <see langword="false"/>.</returns>
+ /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is <see langword="null"/> or empty.</exception>
+ public static bool TryFromJson(string json, out SlowQueryInterceptorTests? value)
+ {
+ ArgumentException.ThrowIfNullOrEmpty(json);
+
+ try
+ {
+ value = JsonSerializer.Deserialize<SlowQueryInterceptorTests>(json, _jsonSerializerOptions);
+ return true;
+ }
+ catch (JsonException)
+ {
+ value = null;
+ return false;
+ }
+ }
 }
